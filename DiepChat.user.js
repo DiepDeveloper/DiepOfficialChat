748dfbdbeefa859a7717503d790fad2a29bd94e6
DiepDeveloper <adam.le.idrissi@gmail.com>
2026-01-22 20:03:24 +0100

    DiepChat.user.js

---
+++ DiepChat.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         DiepChat
+// @namespace    https://tampermonkey.net/
+// @icon         https://diep.chat/assets/GeneralAssets/favicon.png
+// @author       𝗗𝗶𝗲𝗽𝗖𝗵𝗮𝘁 𝗧𝗲𝗮𝗺
+// @version      1.0
+// @description  Beta Version 1.0 Release
+// @match        https://diep.io/*
+// @require      https://diep.chat/DiepChat
+// @run-at       document-start
+// ==/UserScript==
+
+// ================================================== LICENSE ================================================== //
+// © 2026 — @theblueel#0 (Discord)
+// All rights reserved.
+// This code is under exclusive ownership of @theblueel#0 and is protected by copyright.
+
+// 1. Authorization
+// The use of this code is permitted solely for personal, non-commercial purposes, and without any modification.
+
+// 2. Prohibitions
+// It is strictly forbidden, without prior written authorization:
+// to modify, adapt, or create derivative works from this code;
+// to redistribute, share, or publish this code, in whole or in part;
+// to use this code for commercial purposes;
+// to use this code as a basis for a similar or competing project.
+
+// 3. Liability
+// The code is provided "as is", without any warranty.
+// The author cannot be held responsible for any damage resulting from its use.
+
+// 4. Acceptance
+// Any use of this code constitutes full acceptance of this license.
+// In case of non-acceptance, use of this code is prohibited and can lead to legal action.
+// ================================================== LICENSE ================================================== //
